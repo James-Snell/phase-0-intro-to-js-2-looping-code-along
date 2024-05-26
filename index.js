49e9db285e847d61e007a7a1a1adc755205fbc72
James <jmsn311@gmail.com>
2024-05-25 18:51:49 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+let name = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(name){
+    let letter = [];
+    for(let i = 0; i < name.length; i++) {
+        letter.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return letter;
+}
+writeCards()
+
+
+
+function countDown() {
+    let count = 10;
+    while (count >= 0) {
+        console.log(count);
+        count--
+    }
+}
+
+countDown()
